9a1584afc6cec04dbfefd7543eb222748aef6e2a
Yousif Masoud <ymas@deb-bx1.ymasoud.com>
2018-04-29 00:10:13 +0100

    modifies test statement for 'identicalTo' validator

--- test/hypervalidators_spec.ts
+++ test/hypervalidators_spec.ts
@@ -17,7 +17,7 @@ describe('HyperValidators', () => {
     it('should not error if controls are equal',
       () => { expect(HyperValidators.identicalTo(new FormControl(1))(new FormControl(1))).toBeNull(); });
 
-    it('should pass if controls are same value but different types',
+    it('should error if controls are same value but different types',
       () => { expect(HyperValidators.identicalTo(new FormControl('1'))(new FormControl(1))).toEqual({'identicalTo': {'identicalTo': 1, 'actual': '1'}}); });
 
     it('show fail if control values are not equal',
