ac783bf36a2200e7825a0f86d38f3e48f5e57cca
Yousif Masoud <ymas@deb-bx1.ymasoud.com>
2018-04-28 23:56:59 +0100

    converts tabs to spaces

--- src/hypervalidators.ts
+++ src/hypervalidators.ts
@@ -2,23 +2,23 @@ import {Validator, ValidatorFn, ValidationErrors} from '@angular/forms';
 import {AbstractControl} from '@angular/forms';
 
 export class HyperValidators {
-	static equalTo(target:AbstractControl): ValidatorFn {
-		return (control: AbstractControl): ValidationErrors | null => {
-			if (control.value == target.value) {
-				return null;
-			}
+  static equalTo(target:AbstractControl): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (control.value == target.value) {
+        return null;
+      }
 
-			return {'equalTo': {'equalTo': control.value, 'actual':target.value}};
-		}
-	}
-	
-	static identicalTo(target:AbstractControl): ValidatorFn {
-		return (control: AbstractControl): ValidationErrors | null => {
-			if (control.value === target.value) {
-				return null;
-			}
+      return {'equalTo': {'equalTo': control.value, 'actual':target.value}};
+    }
+  }
+  
+  static identicalTo(target:AbstractControl): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (control.value === target.value) {
+        return null;
+      }
 
-			return {'identicalTo': {'identicalTo': control.value, 'actual':target.value}};
-		}
-	}
+      return {'identicalTo': {'identicalTo': control.value, 'actual':target.value}};
+    }
+  }
 }
