6e4edf92d6712b4558cf72148fa37d4275997290
Yousif Masoud <ymas@deb-bx1.ymasoud.com>
2018-04-28 23:38:41 +0100

    initial commit

---
+++ src/hypervalidators.ts
@@ -0,0 +1,24 @@
+import {Validator, ValidatorFn, ValidationErrors} from '@angular/forms';
+import {AbstractControl} from '@angular/forms';
+
+export class HyperValidators {
+	static equalTo(target:AbstractControl): ValidatorFn {
+		return (control: AbstractControl): ValidationErrors | null => {
+			if (control.value == target.value) {
+				return null;
+			}
+
+			return {'equalTo': {'equalTo': control.value, 'actual':target.value}};
+		}
+	}
+	
+	static identicalTo(target:AbstractControl): ValidatorFn {
+		return (control: AbstractControl): ValidationErrors | null => {
+			if (control.value === target.value) {
+				return null;
+			}
+
+			return {'identicalTo': {'identicalTo': control.value, 'actual':target.value}};
+		}
+	}
+}
